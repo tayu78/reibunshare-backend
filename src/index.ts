efbb1347e7ceda770a89360fdd8c432f4f31940a
Yuya Satake <sata.yu.7878@gmail.com>
2022-12-14 23:57:51 -0800

    formatted by prittier

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
-import express from 'express';
-import {json} from 'body-parser'
-import cors from 'cors';
+import express from "express";
+import { json } from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import errorHandler from './middleware/errorHandler';
+import userRoutes from "./routes/userRoutes";
+import errorHandler from "./middleware/errorHandler";
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
-app.use(json())
+app.use(json());
 
-app.use(cors())
+app.use(cors());
 
-app.use(errorHandler)
-
-
+app.use("/api/v1/users", userRoutes);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`This app is running on port ${PORT} !!`)
-})
-
+  console.log(`This app is running on port ${PORT} !!`);
+});
