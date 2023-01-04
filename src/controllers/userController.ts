5d6d06a63e947a3d8f89323b9a99da2df5f42753
Yuya Satake <sata.yu.7878@gmail.com>
2023-01-03 18:13:58 -0800

    added endpoint of managing follow

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from "express";
+import { Types } from "mongoose";
 import User from "../models/user";
 import Book from "../models/book";
 
@@ -22,6 +23,44 @@ export const getUserData: RequestHandler = async (req, res, next) => {
 export const updateUserInfo: RequestHandler = async (req, res, next) => {
   const { user } = req.userData!;
   try {
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const manageFollowing: RequestHandler = async (req, res, next) => {
+  const { user: u } = req.userData!;
+  const { isFollowing } = req.query;
+  const { followingUserId } = req.params;
+
+  try {
+    const user = new User(u);
+
+    const followingUser = await User.findById(followingUserId);
+    if (!followingUser) {
+      return res.status(404).json({
+        message: "User with provided ID does not exist.",
+      });
+    }
+
+    if (isFollowing === "true") {
+      user.following.push(new Types.ObjectId(followingUserId));
+      followingUser.follower.push(user._id);
+    } else {
+      user.following = user.following.filter((id) => {
+        return !id.equals(followingUserId);
+      });
+      followingUser.follower = followingUser.follower.filter((id) => {
+        return !id.equals(user._id);
+      });
+    }
+
+    await user.save();
+    await followingUser.save();
+
+    return res.status(200).json({
+      message: "Managed following successfully!",
+    });
   } catch (err) {
     next(err);
   }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { getUserData } from "../controllers/userController";
+import { getUserData, manageFollowing } from "../controllers/userController";
 import checkAuth from "../middleware/checkAuth";
 
 const router = express.Router();
 
 router.get("/me", checkAuth, getUserData);
+router.put("/:followingUserId/manageFollowing", checkAuth, manageFollowing);
 
 export default router;
